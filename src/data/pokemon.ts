05b891fb794d4758a9f9d1fba184727383dba7ec
Saulo Oliveira <sauloeniefi@gmail.com>
2026-08-18 03:28:20 -0300

    feat: adicionando pokemons pre selecionados adicionando autocomplete na busca adicionando icones

---
+++ src/data/pokemon.ts
@@ -0,0 +1,81 @@
+export interface PokemonFarm {
+    id: string;
+    name: string;
+    value: number;
+    icon: string;
+}
+
+export const pokemonList: PokemonFarm[] = [
+    {
+        id: "croagunk",
+        name: "Croagunk",
+        value: 55,
+        icon: "pokemon/croagunk.png",
+    },
+    {
+        id: "vivillon",
+        name: "Vivillon",
+        value: 38,
+        icon: "pokemon/vivillon.png",
+    },
+    {
+        id: "scatterbug",
+        name: "Scatterbug",
+        value: 20,
+        icon: "pokemon/scatterbug.png",
+    },
+    {
+        id: "chatot",
+        name: "Chatot",
+        value: 32,
+        icon: "pokemon/chatot.png",
+    },
+    {
+        id: "drapion",
+        name: "Drapion",
+        value: 61,
+        icon: "pokemon/drapion.png",
+    },
+    {
+        id: "shieldon",
+        name: "Shieldon",
+        value: 26,
+        icon: "pokemon/shieldon.png",
+    },
+    {
+        id: "magmar",
+        name: "Magmar",
+        value: 16,
+        icon: "pokemon/magmar.png",
+    },
+    {
+        id: "bronzor",
+        name: "Bronzor",
+        value: 13,
+        icon: "pokemon/bronzor.png",
+    },
+    {
+        id: "bronzong",
+        name: "Bronzong",
+        value: 26,
+        icon: "pokemon/bronzong.png",
+    },
+    {
+        id: "gengar",
+        name: "Gengar",
+        value: 27,
+        icon: "pokemon/gengar.png",
+    },
+    {
+        id: "bastiodon",
+        name: "Bastiodon",
+        value: 55,
+        icon: "pokemon/bastiodon.png",
+    },
+    {
+        id: "haunter",
+        name: "Haunter",
+        value: 8,
+        icon: "pokemon/haunter.png",
+    },
+];
